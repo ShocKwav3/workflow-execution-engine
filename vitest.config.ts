64a81507d45808818377d5f4b3dab2ed141f0fda
ShocKwav3 <ahmonemon@gmail.com>
2026-09-10 19:16:34 +0300

    remove vite-tsconfig-path in favor of builtin flag

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,8 +1,9 @@
 import { defineConfig } from "vitest/config";
-import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-  plugins: [tsconfigPaths()],
+  resolve: {
+    tsconfigPaths: true,
+  },
   test: {
     globalSetup: "./packages/core/test/globalSetup.ts",
     projects: [
